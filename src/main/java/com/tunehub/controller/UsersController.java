e0b3dee5ca1f41c507fbdef77f67f23f3c972d17
TheKunwarEmpire <kaushalcool50@gmail.com>
2024-01-17 15:16:52 +0530

    1st version of TuneHub

--- src/main/java/com/tunehub/controller/UsersController.java
+++ src/main/java/com/tunehub/controller/UsersController.java
@@ -1,17 +1,21 @@
 package com.tunehub.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.tunehub.entities.Song;
 import com.tunehub.entities.Users;
+import com.tunehub.services.SongService;
 import com.tunehub.services.UsersService;
 
 import jakarta.servlet.http.HttpSession;
-
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.GetMapping;
 
 
 
@@ -21,6 +25,8 @@ public class UsersController {
 	
 	@Autowired
 	UsersService service;
+	@Autowired
+	SongService songService;
 	@PostMapping("/register")
 	public String addUsers(@ModelAttribute Users user) {
 		boolean user_status = service.emailExists(user.getEmail());
@@ -36,7 +42,7 @@ public class UsersController {
 	@PostMapping("/validate")
 	public String validate(@RequestParam("email") String email,
 			@RequestParam("password") String password, 
-			HttpSession session) {
+			HttpSession session, Model model) {
 		
 		if(service.validateUser(email, password) == true) {
 			String role = service.getRole(email);
@@ -47,6 +53,11 @@ public class UsersController {
 				return "adminHome";
 			}
 			else {
+				Users users = service.getUser(email);
+				boolean userStatus = users.isPremium();
+				model.addAttribute("isPremium", userStatus);
+				List<Song> songsList = songService.fetchAllSongs();
+				model.addAttribute("songs", songsList);
 				return "customerHome";
 			}
 		}
